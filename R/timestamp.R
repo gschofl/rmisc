3fa65bab789d7e276be105151312ae9469010574
Gerhard Schöfl <schwejk2@gmail.com>
2013-07-09 12:07:23 +0200

    make check_timestamp work across different locales

--- R/timestamp.R
+++ R/timestamp.R
@@ -8,11 +8,14 @@ NULL
 #' @param file The local file.
 #' @param .message A message
 #' 
-#' @return \code{FALSE} if the local file is more recent than the remote
-#' source, else \code{TRUE}
+#' @return \code{TRUE} if the remote source is more recent than the local
+#' file, or if the local file does not exist, else \code{FALSE}
 #' @export
 check_timestamp <- function (url, file, .message) {
-  assert_that(is.readable(file))
+  assert_that(is.string(url), is.string(file))
+  if (!file.exists(file)) {
+    return(TRUE)
+  }
   local_time <- file.info(file)$ctime
   
   h <- basicTextGatherer()
@@ -23,15 +26,18 @@ check_timestamp <- function (url, file, .message) {
                      error = function(x) failed <<- TRUE)
   
   if (failed)
-    stop("Failed to connect to ", sQuote(url))
+    stop("Failed to connect to remote source: ", sQuote(url))
   
+  lct <- Sys.getlocale("LC_TIME")
+  Sys.setlocale("LC_TIME", "C")
   remote_time <- grep("Last-Modified: ", usp(h$value(), "\r\n"), value=TRUE)
   remote_time <- as.POSIXct(strptime(sub("Last-Modified: ", "", remote_time),
-                                     format="%a, %d %b %Y %H:%M:%S GMT", tz="GMT"))
+                                     format="%a, %d %b %Y %H:%M:%S", tz="GMT"))
+  Sys.setlocale("LC_TIME", lct)
   
   if (remote_time < local_time) {
     if (.message)
-      message("Local file is more recent than url.")
+      message("Local file is more recent than the remote source.")
     FALSE
   }
   else
